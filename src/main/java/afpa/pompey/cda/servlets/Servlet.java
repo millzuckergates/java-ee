ae29c07003594e977a4c4b61ced995e460b55fcc
millzuckergates <iamdibade@gmail.com>
2023-04-26 11:36:44 +0200

    first commit

---
+++ src/main/java/afpa/pompey/cda/servlets/Servlet.java
@@ -0,0 +1,63 @@
+package afpa.pompey.cda.servlets;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet; 
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest; 
+import jakarta.servlet.http.HttpServletResponse;
+
+/** *
+* @author millzuckergates
+*/
+@WebServlet(urlPatterns = {"/Servlet"}) 
+public class Servlet extends HttpServlet 
+{
+
+    /**
+    * Processes requests for both HTTP <code>GET</code> and <code>POST</code> * methods.
+    *
+    * @param request servlet request
+    * @param response servlet response
+    * @throws ServletException if a servlet-specific error occurs * @throws IOException if an I/O error occurs
+    */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
+    {
+        response.setContentType("text/html;charset=UTF-8"); 
+        try (PrintWriter out = response.getWriter()){
+            /* TODO output your page here. You may use following sample code. */
+            request.getRequestDispatcher("WEB-INF/JSP/index.jsp").forward(request, response);
+        }
+    }
+
+    /**
+    * Handles the HTTP <code>GET</code> method. *
+    * @param request servlet request
+    * @param response servlet response
+    * @throws ServletException if a servlet-specific error occurs
+    * @throws IOException if an I/O error occurs */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
+    {
+        processRequest(request, response);
+    }
+
+    /** 
+    * Handles the HTTP <code>POST</code> method. *
+    * @param request servlet request
+    * @param response servlet response
+    * @throws ServletException if a servlet-specific error occurs
+    * @throws IOException if an I/O error occurs */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException 
+    { 
+        processRequest(request, response);
+    } 
+}
+
+
+
+
+
+
---
+++ src/main/webapp/WEB-INF/JSP/index.jsp
@@ -0,0 +1,5 @@
+<html>
+<body>
+<h2>Hellooooooo</h2>
+</body>
+</html>
